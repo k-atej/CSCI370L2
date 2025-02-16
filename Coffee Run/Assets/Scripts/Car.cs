1d9ce4656a0ad3c9fae8ae1771d8bf42efcabea0
Leigh <johnstonlh@hendrix.edu>
2025-02-16 09:47:20 -0600

    Cars spawn out of "tunnel"
    Movement bug fixed

---
+++ Coffee Run/Assets/Scripts/Car.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Coffee Run/Assets/Scripts/SpawningCars.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SpawningCars : MonoBehaviour
+{
+
+    public GameObject car;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnCar", 5.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+    void SpawnCar()
+    {
+        var position = new Vector2(transform.position.x, transform.position.y); // source 1
+        Instantiate(car, position, Quaternion.identity);
+        
+       
+
+
+    }
+}
