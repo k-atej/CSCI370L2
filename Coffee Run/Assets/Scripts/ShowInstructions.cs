7d9bfa6edf9c7dd0cf0dd0f758c830dcdef0e8f7
Leigh <johnstonlh@hendrix.edu>
2025-02-19 12:54:27 -0600

    Added more Audio

--- Coffee Run/Assets/Scripts/ShowInstructions.cs
+++ Coffee Run/Assets/Scripts/ShowInstructions.cs
@@ -8,13 +8,14 @@ public class ShowInstructions : MonoBehaviour
     public TextMeshProUGUI Instructions;
 
     private Rigidbody2D rb2d;
+    private AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
     
         rb2d = GetComponent<Rigidbody2D>();
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +23,7 @@ public class ShowInstructions : MonoBehaviour
     {
         if(GameManager.Instance.allAreCaffienated == true){
             Instructions.text = "Good Job!! You saved your town! Thanks for playing";
+            audioSource.Play();
         }
         else if(GameManager.Instance.started == true){
             Instructions.text = "There's still people who need your help!";
